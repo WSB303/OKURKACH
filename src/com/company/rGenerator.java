64f6c60c53b8d545a22eba8683d28b0ad25c58ec
Mateusz <c.m97@o2.pl>
2020-06-27 00:06:51 +0200

    Początek/2

--- src/com/company/rGenerator.java
+++ src/com/company/rGenerator.java
@@ -13,15 +13,15 @@ public class rGenerator {
     }
     public void rGenerowanie(){
     Roslina salata = new Roslina("Sałata",5,5,5,5,5,5,5,5,5);
-    rosliny.add();
+    rosliny.add(salata);
     Roslina sorgo = new Roslina("Sorgo",5,5,5,5,5,5,5,5,5);
-    rosliny.add();
+    rosliny.add(sorgo);
     Roslina kukurydza = new Roslina("Kukurydza",5,5,5,5,5,5,5,5,5);
-    rosliny.add();
+    rosliny.add(kukurydza);
     Roslina jablon = new Roslina("Jabłoń",5,5,5,5,5,5,5,5,5);
-    rosliny.add();
+    rosliny.add(jablon);
     Roslina rukola = new Roslina("Rukola",5,5,5,5,5,5,5,5,5);
-    rosliny.add();
+    rosliny.add(rukola);
     }
 
 }
