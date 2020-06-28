5c057533b2ca9b413d8dbcec5269b81659141fe3
Mateusz <c.m97@o2.pl>
2020-06-28 23:36:36 +0200

    3

--- src/com/company/jednostki/Roslina.java
+++ src/com/company/jednostki/Roslina.java
@@ -52,9 +52,9 @@ public class Roslina {
 
     public String Wypisz()
     {
-        return "Nazwa:" + nazwa + " cena za sadzonke:" + cenaRosliny + " Koszt sadzenia: " + kosztSadzenia + " Koszt ochrony:" +
-                ochrona + " Wydajnosc:" + wydajnosc + " Dlugosc do zbioru:" + czasZbioru +
-                " Tygodnie siania: od " + okresOd + " do " + okresDo + " Koszt zbioru:" + cenaZbioru + " Cena skupu:" + cenaSkupu;
+        return "Nazwa:" +nazwa+ " cena za sadzonke:" +cenaRosliny+ " Koszt sadzenia: " +kosztSadzenia+ " Koszt ochrony:"+
+                ochrona+ " Wydajnosc:" +wydajnosc+ " Dlugosc do zbioru:" +czasZbioru+
+                " Tygodnie siania: od " +okresOd+ " do " +okresDo+ " Koszt zbioru:" +cenaZbioru+ " Cena skupu:" +cenaSkupu;
     }
 
 }
--- src/com/company/jednostki/zwierze.java
+++ src/com/company/jednostki/zwierze.java
@@ -4,6 +4,45 @@ public class zwierze {
     public int Cena;
     public String Nazwa;
     public int masa;
-    public int masaNaTydzien;
+    public int obecnaMasa;
+    public int doroslosc;
+    public int aktualnyTydzien;
+    public int jedzenie;
+    public int szansaRomnozenia;
+    public Budynek Budynek;
+    public RodzajPozywienia rodzajPozywienia;
+    public zwierze(int Cena, String Nazwa,
+                   int masa, int obecnaMasa,
+                   int doroslosc,int aktualnyTydzien,
+                   int jedzenie, int szansaRomnozenia,
+                   Budynek Budynek,RodzajPozywienia RodzajPozywienia){
+        this.Cena = Cena;
+        this.Nazwa = Nazwa;
+        this.masa = masa;
+        this.doroslosc = doroslosc;
+        this.jedzenie = jedzenie;
+        this.szansaRomnozenia = szansaRomnozenia;
+        this.Budynek = Budynek;
+        this.rodzajPozywienia = RodzajPozywienia;
+        obecnaMasa = 0;
+        aktualnyTydzien = 0;
+    }
+    public zwierze(){
 
+    }
+    public String Wypisz()
+    {
+        return "Nazwa: "+Nazwa+" Cena: "+Cena+" Dobor masy: "+masa+" Czas doroslosci: "+doroslosc+" Ilosc jedzenia: "
+                +jedzenie+" Szansa na rozmnozenie: "+szansaRomnozenia+"% Aktualny tydzień: "+aktualnyTydzien;
+    }
+    public zwierze Kopiuj()
+    {
+        return this;
+    }
+    public static enum RodzajPozywienia
+    {
+        rosliny,
+        mieso
+    }
 }
+
